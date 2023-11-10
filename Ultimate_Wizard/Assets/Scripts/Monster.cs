6d2f5ec3bea77c37588ac60902a34b060abf33fe
sforseohn <sforseohn@ewhain.net>
2023-11-11 02:02:36 +0900

    Add Monster Pattern 1

--- Ultimate_Wizard/Assets/Scripts/Monster.cs
+++ Ultimate_Wizard/Assets/Scripts/Monster.cs
@@ -6,14 +6,19 @@ public class Monster : MonoBehaviour
 {
     [SerializeField]
     private float health = 1000f;
+
     [SerializeField]
     private float speed = 1f;
+
     [SerializeField]
     private float delay = 0.2f; // 기준 딜레이
-    public float curDelay;      // 현재 딜레이 시간
+    public float curDelay;        // 현재 딜레이 시간
 
     [SerializeField]
     private GameObject bullet;
+
+    [SerializeField]
+    private GameObject player;
 
     void Start()
     {
@@ -22,9 +27,8 @@ public class Monster : MonoBehaviour
 
     void Update()
     {
-        if (health <= 0)
+        if (health <= 0) // 플레이어의 승리
         {
-            // 플레이어의 승리
             Win();
         }
 
@@ -32,14 +36,10 @@ public class Monster : MonoBehaviour
         Reload();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void Reload()
     {
-        if (collision.gameObject.CompareTag("PlayerBullet"))
-        {
-            health -= 1f;
-        }
+        curDelay += Time.deltaTime;
     }
-
 
     void Attack(int pattern)
     {
@@ -63,26 +63,48 @@ public class Monster : MonoBehaviour
     // 몬스터 공격 패턴 1 - 홀수형
     void AttackPattern1()
     {
-        int lines = 3;
+        int lines = 7;
+        float angleDiff = 20f; // 중심으로부터의 각도
 
         // 총알 생성
-        GameObject b = Instantiate(bullet, transform.position, transform.rotation);
-        Rigidbody2D rigid = b.GetComponent<Rigidbody2D>();
+        for (int i = 0; i < lines; i++)
+        {
+            GameObject b = Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody2D rigid = b.GetComponent<Rigidbody2D>();
 
-        // 총알 움직임
-        rigid.AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+            // 총알 방향
+            Vector2 center = (Vector2)player.transform.position - (Vector2)transform.position;
+            Vector2 direction;
 
+            // 중간 갈래는 플레이어를 향하도록 설정
+            if (i == lines / 2)
+            {
+                direction = center;
+            }
+            else
+            {
+                // 나머지 갈래는 중심으로부터 일정 각도씩 차이 나도록 설정
+                float angle = (i < lines / 2) ? i * angleDiff : (i - lines / 2) * - angleDiff;
+                direction = Quaternion.Euler(0, 0, angle) * center.normalized;
+            }
+
+            rigid.AddForce(direction.normalized * 10, ForceMode2D.Impulse);
+        }
     }
 
-    void Reload()
+    private void OnCollisionEnter(Collision collision)
     {
-        curDelay += Time.deltaTime;
+        if (collision.gameObject.CompareTag("PlayerBullet"))
+        {
+            Minus();
+        }
     }
+
 
     void Minus()
     {
         // 플레이어로부터 오는 공격 처리
-
+        health -= 1f;
     }
 
     void Win()
