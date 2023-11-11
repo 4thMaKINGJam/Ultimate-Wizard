0a45ade0a0c16c4bb665b239e33a2c136370cb79
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2023-11-11 22:16:58 +0900

    dummy change

--- Ultimate_Wizard/Assets/Scripts/GameManager.cs
+++ Ultimate_Wizard/Assets/Scripts/GameManager.cs
@@ -8,19 +8,41 @@ public class GameManager : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject player;
+    public Player player;
+
+    [SerializeField]
+    private dummy dummy;
+
+    private textchange textChangeInstance;
 
     public Image[] lifeImage;
     public Sprite fulllife;
     public Sprite emptylife;
-    public GameObject gameOverSet;
+    public Sprite Sonic_h;
+    public Sprite Hercules_h;
+    public Sprite Zombie_h;
+    public Sprite Sonic;
+    public Sprite Hercules;
+    public Sprite Zombie;
 
-    public GameObject GamoverUI;
+    //   public GameObject gameOverSet;
+
+    Sprite selectedSprite = null;
+
+    Sprite selectedSpritebody = null;
+
+    //  public GameObject GamoverUI;
+
+    public GameObject EvolutionUI;
 
     public static GameManager instance = null;
 
     private void Awake()
+
     {
+        textChangeInstance = FindObjectOfType<textchange>();
+
+
         if (instance == null)
         {
             instance = this;
@@ -34,15 +56,72 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        gameOverSet.SetActive(false);
-        GamoverUI.SetActive(false);
+
+        EvolutionUI.SetActive(false);
+    //    gameOverSet.SetActive(false);
+    //   GamoverUI.SetActive(false);
     }
 
 
     private void Update()
     {
+      
+    }
+    public void textchange_(int candidates)
+    {
+
+        dummy.changeText(candidates);
+
+    }
+    public void SelectDummy(int candidates)
+    {
+        player = GameObject.FindObjectOfType<Player>();
+
+        switch (candidates)
+            {
+
+            case 0:
+                selectedSprite = Sonic_h;
+                selectedSpritebody = Sonic;
+                dummy.ChangeDummyImage(selectedSpritebody);
+                Evolution_dummy();
+                //    player.GetComponent<Player>().speed = 5.0f;
+
+                break;
+            case 1:
+                selectedSprite = Hercules_h;
+                selectedSpritebody = Hercules;
+            //    player.GetComponent<Player>().power = 2.0f;
+                dummy.ChangeDummyImage(selectedSpritebody);
+                Evolution_dummy();
+                break;
+            case 2:
+                selectedSprite = Zombie_h;
+                selectedSpritebody = Zombie;
+                dummy.ChangeDummyImage(selectedSpritebody);
+                Evolution_dummy();
+                //   player.GetComponent<Player>().life = 4;
+                break;
+            
+            default:
+                selectedSprite = Sonic_h;
+                selectedSpritebody = Sonic;
+                dummy.ChangeDummyImage(selectedSpritebody);
+                Evolution_dummy();
+                // player.GetComponent<Player>().speed = 5.0f;
+                break;
+
+        }
+
+        dummy.GetComponent<dummy>().MoveToPosition(0f, 0f);
+
+        if (selectedSprite != null)
+        {
 
 
+            // 플레이어의 이미지 변경
+          //  player.GetComponent<Image>().sprite = selectedSprite;
+        }
     }
 
     public void UpdateLifeIcon(int life)
@@ -62,14 +141,25 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0f;
         Debug.Log("GameManager 호출 성공");
     }
+    public void Evolution_dummy()
+    {
+        EvolutionUI.SetActive(true);
+    }
+
+    public void Letsgo()
+    {
+        SceneManager.LoadScene(7);
+    }
 
     public void GameOver()
     {
         Time.timeScale = 0f;
-        gameOverSet.SetActive(true);
-        GamoverUI.SetActive(true);
+   //     gameOverSet.SetActive(true);
+   //     GamoverUI.SetActive(true);
     }
 
+
+ 
     public void GameRetry()
     {
         Time.timeScale = 1f;
