34c0ec7b49395df0d1b06bb0baf300e0f09130b7
sforseohn <100199530+sforseohn@users.noreply.github.com>
2023-11-11 13:37:15 +0900

    Merge main into Monster_P6_7 (#5)
    * Feat: 메인 -> 스토리 장면 전환  페이드 인 효과 적용
    
    * Feta: Start, Story 장면 완료
    
    * Merge Main
    
    ---------
    
    Co-authored-by: OSSU <o.ss.u2056@gmail.com>

--- Ultimate_Wizard/Assets/Scripts/DialogueSystem.cs
+++ Ultimate_Wizard/Assets/Scripts/DialogueSystem.cs
@@ -14,10 +14,16 @@ public class Dialogue {
 
 public class DialogueSystem : MonoBehaviour
 {
+    [SerializeField] private Image image; // 페이드아웃 효과에 쓰일 이미지
+    [SerializeField] private float delay_time = 1.5f;
+    [SerializeField] private Animator anim; // 대화창 open, close 애니메이터
+    [SerializeField] private string scene_name;
+
     [SerializeField] private Text character_name; // 캐릭터 이름
     [SerializeField] private Text sentence; // 대화 내용
     [SerializeField] private SpriteRenderer sprite_character; // 캐릭터 이미지 제어 변수
     [SerializeField] private Image dialogue_box; // 다이얼로그 박스 제어 변수
+    [SerializeField] private GameObject button_ui; // 상단 건너뛰기 & 끝내기 버튼
 
     private bool isDialogue = false; // 대화가 진행중인지
     private int count = 0; // 대화 얼마나 진행되었는지
@@ -25,7 +31,45 @@ public class DialogueSystem : MonoBehaviour
     [SerializeField] private Dialogue[] dialogues;
     public string text = "";
 
+    // 초기 세팅
+    private void Awake() {
+        sentence.text = "";
+        character_name.text = "";
+    }
+
     private void Start() {
+        Debug.Log("2_Story 진행 시작");
+        // 대화 화면 비활성화
+        StartDialogue(false);
+        sprite_character.gameObject.SetActive(false);
+        // 배경 이미지 페이드 인 효과
+        StartCoroutine("FadeOutCoroutine");
+    }
+
+    IEnumerator FadeOutCoroutine() {
+        // 처음 알파값
+        float fadeCount = 1;
+
+        while(fadeCount > 0.0f) {
+            fadeCount -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            image.color = new Color(0, 0, 0, fadeCount);
+        }
+
+        // 다음 애니메이션 전 딜레이
+        yield return new WaitForSeconds(0.15f);
+        sprite_character.gameObject.SetActive(true);
+        
+        // 다음 애니메이션 전 딜레이
+        yield return new WaitForSeconds(delay_time);
+        // 대화창 애니메이션 시작
+        anim.SetBool("isOpen", true);
+
+        // 다음 애니메이션 전 딜레이
+        yield return new WaitForSeconds(delay_time);
+        StopCoroutine("FadeOutCoroutine");
+
+        // 다이얼로그 시작
         ShowDialogue();
     }
 
@@ -33,13 +77,28 @@ public class DialogueSystem : MonoBehaviour
         // 대화 시작
         StartDialogue(true);
         count = 0;
+
         // 다음 대화 진행
         StartCoroutine("Next");
     }
 
+    public void EndDialogue() {
+        StopCoroutine("Next");
+        // 텍스트 초기화
+        sentence.text = "";
+        character_name.text = "";
+
+        // 대화 종료
+        StartDialogue(false);
+        anim.SetBool("isOpen", false);
+
+        // 화면 전환
+        StartCoroutine(FadeInCoroutine());
+    }
+
     private void StartDialogue(bool flag) {
         sprite_character.gameObject.SetActive(flag);
-        dialogue_box.gameObject.SetActive(flag);
+        button_ui.SetActive(flag);
         isDialogue = flag;
     }
 
@@ -52,19 +111,20 @@ public class DialogueSystem : MonoBehaviour
             // 캐릭터 이미지
             sprite_character.sprite = dialogues[count].image;
             
-
             // 텍스트 타이핑 효과
             string s = dialogues[count].dialogue;
             count++;
             text = "";
+            sentence.text = "";
+            yield return new WaitForSeconds(0.3f);
 
             for (int i = 0; i < s.Length; i++) {
                 text += s[i];
                 sentence.text = text;
                 yield return new WaitForSeconds(0.05f);
             }
-        } else {
-            StartDialogue(false); // 대화 종료
+        } else { // 모든 스크립트 출력 끝나면 다음 장면으로 넘어감
+            EndDialogue();
         }
     }
 
@@ -86,6 +146,22 @@ public class DialogueSystem : MonoBehaviour
     }
 
     public void Skip() {
-        SceneManager.LoadScene("2_DummySelect");
+        StopCoroutine("Next");
+        EndDialogue();
+    }
+
+    IEnumerator FadeInCoroutine() {
+        // 처음 알파값
+        float fadeCount = 0;
+
+        while(fadeCount < 1.0f) {
+            fadeCount += 0.01f;
+            yield return new WaitForSeconds(0.01f); // 0.01초마다 실행
+            image.color = new Color(0, 0, 0, fadeCount);
+        }
+
+        StopCoroutine("FadeInCoroutine");
+        // 화면 전환
+        SceneManager.LoadScene(scene_name);
     }
 }
